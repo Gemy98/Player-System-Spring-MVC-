7ec47c03991183179fbac148c0288e94566db455
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-11 04:29:25 +0300

    Create DAO Service Controller

--- src/main/java/com/spring/dao/PlayerDaoImpl.java
+++ src/main/java/com/spring/dao/PlayerDaoImpl.java
@@ -1,5 +1,8 @@
 package com.spring.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class PlayerDaoImpl implements PlayerDAO {
 
 }
--- src/main/java/com/spring/service/PlayerServiceImpl.java
+++ src/main/java/com/spring/service/PlayerServiceImpl.java
@@ -1,5 +1,8 @@
 package com.spring.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlayerServiceImpl implements PlayerService {
 
 }
