e9759b738791eb57cda68d141f90b0ae943c5281
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-11 04:47:09 +0300

    getPlayers dao

--- src/main/java/com/spring/dao/PlayerDaoImpl.java
+++ src/main/java/com/spring/dao/PlayerDaoImpl.java
@@ -1,8 +1,35 @@
 package com.spring.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.spring.model.Player;
 
 @Repository
 public class PlayerDaoImpl implements PlayerDAO {
+
+	
+	private SessionFactory sessionFactory ; 
+	
+	
+	@Autowired
+	public PlayerDaoImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+ 
+
+	@Override
+	public List<Player> getPlayers() {
+		Session session = sessionFactory.getCurrentSession();
+		
+		Query<Player> query = session.createQuery("from player",Player.class);
+		return query.getResultList();
+	}
 
 }
