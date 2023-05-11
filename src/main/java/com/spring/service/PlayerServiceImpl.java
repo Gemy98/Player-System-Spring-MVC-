3eda72d4924f0a21aed3beef2091d1b7b41f8678
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-11 04:52:07 +0300

    AllPlayerService

--- src/main/java/com/spring/service/PlayerServiceImpl.java
+++ src/main/java/com/spring/service/PlayerServiceImpl.java
@@ -1,8 +1,29 @@
 package com.spring.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.spring.dao.PlayerDAO;
+import com.spring.model.Player;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
+
+	private PlayerDAO playerDao ; 
+	
+	@Autowired
+	public PlayerServiceImpl(PlayerDAO playerDao) {
+		this.playerDao = playerDao;
+	}
+
+	@Override
+	public List<Player> allPlayers() {
+
+		
+		
+		return playerDao.getPlayers();
+	}
 
 }
